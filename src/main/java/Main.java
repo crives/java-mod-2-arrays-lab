e89722e46040d48ae8908828ceb5d9b0f144a03f
Caroline Rives <car.rives@gmail.com>
2022-06-27 17:39:29 -0400

    Array lab submission

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner inputScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // your code here
+        String[] students = new String[10];
+        
+        System.out.println("Please enter the names of 10 students below:");
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("Enter student's name:");
+            String str = inputScanner.nextLine();
+            students[i] = str;
+        }
+
+        System.out.println("You have entered the following names:");
+
+        for (String student : students) {
+            System.out.println(student);
+        }
     }
 }
